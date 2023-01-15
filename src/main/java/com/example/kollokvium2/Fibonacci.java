815ff6b3ca383b254740f859b029dc7f98c86565
KirSivogr <kirillsivograkovv@mail.ru>
2023-01-15 16:33:54 +0300

    add Fibonacci tests

--- src/main/java/com/example/kollokvium2/Fibonacci.java
+++ src/main/java/com/example/kollokvium2/Fibonacci.java
@@ -8,10 +8,13 @@ public class Fibonacci {
         int n0 = 1;
         int n1 = 1;
         int n2 = 0;
+        if (n == 0) {
+            return 0;
+        }
         if (n == 1 || n == 2) {
             return 1;
         }
-        for(int i = 3; i <= n; i++){
+        for (int i = 3; i <= n; i++) {
             n2 = n0 + n1;
             n0 = n1;
             n1 = n2;
@@ -25,6 +28,9 @@ public class Fibonacci {
         int n1 = 1;
         int n2 = 0;
         fib.add(1);
+        if (n == 0) {
+            return fib;
+        }
         if (n == 1) {
             return fib;
         }
@@ -32,24 +38,12 @@ public class Fibonacci {
         if (n == 2) {
             return fib;
         }
-        for(int i = 3; i <= n; i++){
+        for (int i = 3; i <= n; i++) {
             n2 = n0 + n1;
             fib.add(n2);
             n0 = n1;
             n1 = n2;
         }
         return fib;
-    }
-
-    public int getNNumberFibRec(int n) {
-        if (n == 0){
-            return 0;
-        }
-        if (n == 1){
-            return 1;
-        }
-        else{
-            return getNNumberFibRec(n - 1) + getNNumberFibRec(n - 2);
-        }
     }
 }
